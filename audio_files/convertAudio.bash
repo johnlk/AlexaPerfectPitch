e14f6b30acbb6a656ed2e7338fae08a8ff7ae4b5
johnlk <jlkuhn@purdue.edu>
2018-02-05 22:42:38 -0500

    added more audio files

--- audio_files/convertAudio.bash
+++ audio_files/convertAudio.bash
@@ -1,3 +1,3 @@
 for file in ./*; do
-	ffmpeg -i "$file" -ac 2 -filter:a "volume=40dB" -b:a 48k -ar 16000 ./"${file%.*}".mp3
+	ffmpeg -i "$file" -ac 2 -b:a 48k -ar 16000 ./mp3s/"${file%.*}".mp3
 done
